bdef4e173f22a46c26b9baf02a27bad18e57810b
常洺瑋 (Arthur) <arthurc0102@gmail.com>
2020-11-02 15:52:20 +0800

    Update centos-docker.sh

--- centos-docker.sh
+++ centos-docker.sh
@@ -2,20 +2,20 @@ set -e
 
 echo "Install docker"
 
-sudo yum remove docker \
-                docker-client \
-                docker-client-latest \
-                docker-common \
-                docker-latest \
-                docker-latest-logrotate \
-                docker-logrotate \
-                docker-engine
+sudo yum remove -y docker \
+                   docker-client \
+                   docker-client-latest \
+                   docker-common \
+                   docker-latest \
+                   docker-latest-logrotate \
+                   docker-logrotate \
+                   docker-engine
 
 sudo yum install -y yum-utils
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 sudo systemctl enable --now docker
 
